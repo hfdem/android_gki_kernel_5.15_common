89728f4bef63f1eaf0b392fb5b266e6a5f01bf2b
Qais Yousef <qyousef@layalina.io>
2024-03-24 00:45:49 +0000

    sched/topology: Export asym_cap_list
    So that we can use it to iterate through available capacities in the
    system. Sort asym_cap_list in descending order as expected users are
    likely to be interested on the highest capacity first.
    
    Make the list RCU protected to allow for cheap access in hot paths.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20240324004552.999936-2-qyousef@layalina.io

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -104,6 +104,20 @@ extern long calc_load_fold_active(struct rq *this_rq, long adjust);
 
 extern void call_trace_sched_update_nr_running(struct rq *rq, int count);
 /*
+ * Asymmetric CPU capacity bits
+ */
+struct asym_cap_data {
+	struct list_head link;
+	struct rcu_head rcu;
+	unsigned long capacity;
+	unsigned long cpus[];
+};
+
+extern struct list_head asym_cap_list;
+
+#define cpu_capacity_span(asym_data) to_cpumask((asym_data)->cpus)
+
+/*
  * Helpers for converting nanosecond timing to jiffy resolution
  */
 #define NS_TO_JIFFIES(TIME)	((unsigned long)(TIME) / (NSEC_PER_SEC / HZ))
