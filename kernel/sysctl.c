23cd6a386b483de475a9d55170a3d7239126216c
ztc1997 <dev@ztc1997.com>
2024-09-13 14:56:28 +0800

    sysctl: Make swappiness read only

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2727,6 +2727,13 @@ static struct ctl_table kern_table[] = {
 	{ }
 };
 
+static int proc_dointvec_minmax_ro_wrapper(struct ctl_table * table, int write, void __user * buffer, size_t * lenp, loff_t * ppos){
+	if (write)
+		return 0;
+
+	return proc_dointvec_minmax(table, write, buffer, lenp, ppos);
+}
+
 static struct ctl_table vm_table[] = {
 	{
 		.procname	= "overcommit_memory",
@@ -2844,7 +2851,7 @@ static struct ctl_table vm_table[] = {
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
+		.proc_handler	= proc_dointvec_minmax_ro_wrapper,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_TWO_HUNDRED,
 	},
