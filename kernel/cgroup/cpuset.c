2b1e3c530c51b4c1765e5699ba6a79aa1deddd9e
hfdem <admin@hfdem.com>
2024-09-22 05:23:22 +0800

    kernel: Tune cpusets assist values

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2620,12 +2620,7 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 #ifdef CONFIG_CPUSETS_ASSIST
 	static struct cs_target cs_targets[] = {
 		/* Little-only cpusets go first */
-		{ "foreground",		"0-5" },
-		{ "background",		"0-2" },
-		{ "system-background",	"0-3" },
-		{ "restricted",		"0-5" },
-		{ "top-app",		"0-7" },
-		{ "camera-daemon",	"0-3,6-7" },
+		{ "foreground",		"0-6" },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
