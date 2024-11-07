5878de6d001e324e9216c14321621c0b5b3104ca
hfdem <admin@hfdem.com>
2024-11-07 16:33:41 +0800

    kernel: module: Add binder_prio to the custom blacklist

--- kernel/module.c
+++ kernel/module.c
@@ -3592,6 +3592,9 @@ static char *custom_module_blacklist[] = {
 #if IS_BUILTIN(CONFIG_ZSMALLOC)
     "zsmalloc",
 #endif
+#if IS_BUILTIN(CONFIG_BINDER_PRIO)
+    "binder_prio",
+#endif
 #ifdef CONFIG_MACH_XIAOMI_MARBLE
     /* Not required */
     "qca6750", "icnss2", "cs35l41_dlkm", "atmel_mxt_ts", "focaltech_fts", "nt36xxx_i2c", "nt36xxx_spi", "synaptics_dsx",
