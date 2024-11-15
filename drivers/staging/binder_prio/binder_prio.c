0315481e130298fd13b80518dfc85978b5b334f5
Pzqqt <821026875@qq.com>
2024-11-15 21:57:30 +0800

    drivers: staging: binder_prio: Do not increase the priority of the surfaceflinger thread
    The surfaceflinger thread is very common, and always giving it
    higher priority will increase power consumption.

--- drivers/staging/binder_prio/binder_prio.c
+++ drivers/staging/binder_prio/binder_prio.c
@@ -18,7 +18,7 @@ module_param(debug, uint, 0644);
 static const char *task_name[] = {
 	"com.miui.home",
 	"ndroid.systemui",  // com.android.systemui
-	"surfaceflinger",
+	// "surfaceflinger",
 	"cameraserver",
 	"camerahalserver",
 	"rsonalassistant",  // com.miui.personalassistant
